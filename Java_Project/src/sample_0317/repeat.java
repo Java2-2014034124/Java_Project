aa211bf4d0b2800070ee4a6da8c7290e7590c4ec
Moonseunggi <moon44432@naver.com>
2016-03-17 21:27:58 +0900

    오늘 열심히했다

---
+++ Java_Project/src/sample_0317/repeat.java
@@ -0,0 +1,40 @@
+package sample_0317;
+
+public class repeat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i<=2; i++)
+			for(int j = 1; j<=3; j++){
+				System.out.print(i);
+				System.out.println(j);
+			}
+
+	
+	
+	for(int b =1;b<=9;b++){
+		for(int a =2;a<=9;a++){
+			System.out.print(a);
+			System.out.print("*");
+			System.out.print(b);
+			System.out.print("=");
+			System.out.print(a*b);
+			System.out.print(" ");
+			
+
+		}
+		System.out.println(" ");
+	}
+	
+	for(int i=8; i>=1; i--){
+		for(int j=1;j<=9;j++){
+			System.out.print(i*1000+j);
+			System.out.print(" ");
+
+}
+	System.out.println(" ");
+	
+	}}}
+	
+	
